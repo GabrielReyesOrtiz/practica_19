d7fdc639ed9a00bab0212bedfb9615b99ef3eb3f
GabrielReyesOrtiz <greyes17@ucol.mx>
2022-05-21 18:34:52 -0500

    Practica_19_SegundaParte

--- routes/persons.js
+++ routes/persons.js
@@ -11,5 +11,20 @@ router.get('/persons', function(req, res, next){
     res.json(persons);
   });
 });
+//Agregamos ruta por GET para renderizar la vista 
+router.get('/person', function (req, res){
+  res.render('person');
+});
+//Agregamos nueva ruta por POST para poder agregar un docuento nuevo a nuetsra coleccion
+router.post('/addPerson', function(req, res){
+  //nueva instancia de "Person" que recibe valores del body
+  const myPerson = new Person({
+    nombre: req.body.nombre,
+    edad: req.body.edad,
+    tipoSangre: req.body.tipoSangre,
+    nss: req.body.nss}); //Creamos la entidad 
+    myPerson.save();//guardamos en BD
+});
+
 //Exportamos el router
 module.exports=router;
